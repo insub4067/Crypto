874ee3678e1272496e7339659e14d50708c0468b
insub4067 <vb15@kakao.com>
2023-01-11 23:05:02 +0900

    File : Assets and Color Extension

--- Crypto/ContentView.swift
+++ Crypto/ContentView.swift
@@ -9,13 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color.theme.background
+                .ignoresSafeArea()
+            VStack {
+                
+            }
         }
-        .padding()
     }
 }
 
---
+++ Crypto/Extensions/Color+.swift
@@ -0,0 +1,24 @@
+//
+//  Color.swift
+//  Crypto
+//
+//  Created by Kim Insub on 2023/01/11.
+//
+
+import SwiftUI
+
+extension Color {
+
+    static let theme = ColorTheme()
+
+}
+
+struct ColorTheme {
+
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+    
+}
