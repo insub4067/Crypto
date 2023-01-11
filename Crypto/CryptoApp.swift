4ca6e1e6de574b12fd9b36d850335af80a2eb772
insub4067 <vb15@kakao.com>
2023-01-11 22:56:35 +0900

    init

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Kim Insub on 2023/01/11.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
